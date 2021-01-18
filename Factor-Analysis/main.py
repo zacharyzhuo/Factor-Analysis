7866f7547a145bb2cdab719d9f1b41cfa2936bb9
zachary zhuo <courage0322@gmail.com>
2021-01-18 11:59:39 +0800

    update readme

--- Factor-Analysis/main.py
+++ Factor-Analysis/main.py
@@ -6,15 +6,16 @@ from modules.mystrategy import MyStrategy
 from modules.analysis import Analysis
 
 
-# factor_list = ['GVI', 'EPS', 'MOM', 'PE', 'EV_EBITDA', 'EV_S', 'FC_P', 'CROIC', 'FC_OI', 'FC_LTD']
+factor_list = ['GVI', 'EPS', 'MOM', 'PE', 'EV_EBITDA', 'EV_S', 'FC_P', 'CROIC', 'FC_OI', 'FC_LTD']
 # 0: equal_weight; 1: equal_risk(ATR); 2: equal_risk(SD)
 weight_setting = [0, 1, 2]                  
 n_season = [1, 2]
-# group = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# position = [5, 10, 15, 30, 90, 150]
-factor_list = ['PE', 'EV_EBITDA', 'EV_S', 'GVI']
-group = [1, 2, 3, 4, 5, 6]
-position = [5, 10, 15, 30, 90]
+group = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+position = [5, 10, 15, 30, 90, 150]
+
+# factor_list = ['PE', 'EV_EBITDA', 'EV_S', 'GVI']
+# group = [1, 2, 3, 4, 5, 6]
+# position = [5, 10, 15, 30, 90]
 
 start_equity = 10000000
 start_date = '2010-01-01'
@@ -40,32 +41,33 @@ end_date = '2017-12-31'
 # print("Execution time: %f second" % (end - start))
 
 
-cal = Calendar('TW')
-for factor in factor_list:
-    fac = Factor([factor])
-    for gro in group:
-        for pos in position:
-            # try:
-            start = time.time()
-            strategy_config = {
-                'factor_list': [factor],
-                'weight_setting': weight_setting[0],
-                'n_season': n_season[0],
-                'group': gro,
-                'position': pos,
-                'start_equity': start_equity,
-                'start_date': start_date,
-                'end_date': end_date,
-            }
-            my_stra = MyStrategy(strategy_config, cal, fac)
-            end = time.time()
-            print("Execution time: %f second" % (end - start))
-            # except Exception as e:
-            #     print(e)
-            #     pass  
+# cal = Calendar('TW')
+# for factor in factor_list:
+#     fac = Factor([factor])
+#     for gro in group:
+#         for pos in position:
+#             try:
+#                 start = time.time()
+#                 strategy_config = {
+#                     'factor_list': [factor],
+#                     'weight_setting': weight_setting[0],
+#                     'n_season': n_season[0],
+#                     'group': gro,
+#                     'position': pos,
+#                     'start_equity': start_equity,
+#                     'start_date': start_date,
+#                     'end_date': end_date,
+#                 }
+#                 my_stra = MyStrategy(strategy_config, cal, fac)
+#                 end = time.time()
+#                 print("Execution time: %f second" % (end - start))
+#             except Exception as e:
+#                 print(e)
+#                 pass  
 
 
-# analysis = Analysis(start_equity, start_date, end_date)
+analysis = Analysis(start_equity, start_date, end_date)
+analysis.analysis_factor_performance(factor_list[0])
 # analysis.anslysis_portfolio()
 # analysis.rank_portfolio_return()
 # analysis.plot_net_profit_years()
--- README.md
+++ README.md
@@ -1,31 +1,35 @@
-## 虛擬環境
-```
-cd C:\Users\IPLAB2\Documents\GitHub\Factor-Analysis
-.\venv\Scripts\activate
-cd Factor-Analysis
-
-source ./venv/bin/activate
-deactivate
-```
+# 安裝虛擬環境
 ```
 python -m venv venv
 pip freeze > requirements.txt
 python -m pip install -r requirements.txt
 ```
 
-## For Windows
+# For Windows
+### install TA-Lib
 ```
 pip3 install C:\Users\IPLAB2\Documents\GitHub\Factor-Analysis\Factor-Analysis\TA-Lib\TA_Lib-0.4.19-cp37-cp37m-win_amd64.whl
 ```
+### virtualenv
+```
+cd C:\Users\IPLAB2\Documents\GitHub\Factor-Analysis
+.\venv\Scripts\activate
+cd Factor-Analysis
 
-## For Mac
+deactivate
+```
+
+# For Mac
+### install TA-Lib
 ```
 brew install ta-lib
 pip install TA-lib
 ```
-
-## 執行test中的程式
+### virtualenv
 ```
-python3 test.test_mom
-python3 test.test_ranking
+cd /Users/zachary/Documents/GitHub/Factor-Analysis
+source ./venv/bin/activate
+cd Factor-Analysis
+
+deactivate
 ```
